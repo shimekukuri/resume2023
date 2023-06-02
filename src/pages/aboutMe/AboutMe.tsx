9851a81f79a1170492a00a9f93f7ccd5b13e24ed
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-06-02 10:19:04 -0400

    -

---
+++ src/pages/aboutMe/AboutMe.tsx
@@ -0,0 +1,9 @@
+import { Parallax } from 'react-scroll-parallax';
+
+export default function AboutMe() {
+  return (
+    <div className="h-screen overflow-y-scroll overflow-x-clip">
+      <div></div>
+    </div>
+  );
+}
