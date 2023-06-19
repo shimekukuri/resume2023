919525b349118c9439a06e6479e53e09f3241c8e
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-06-19 13:42:24 -0400

    -

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -9,8 +9,8 @@ export default function Blog() {
       <div className="absolute bottom-0 @[720px]:top-0 h-16  w-full backdrop-blur-md peer-checked/navid:bg-blue-100 bg-opacity-10 peer-checked/navid:animate-height-screen">
         Nav
       </div>
-      <div className="absolute hidden bottom-0 right-0 left-0 top-0 bg-red-500 peer-checked/navid:block">
-        d
+      <div className="absolute hidden bottom-0 right-0 left-0 top-0 backdrop-blur-sm peer-checked/navid:flex p-4 md:p-56">
+        <div className="flex-1 border-2 border-white bg-white bg-opacity-50 rounded-lg  shadow-2xl backdrop-blur-xl"></div>
       </div>
       <div className="flex flex-col flex-1 gap-4 bg-green-100 overflow-y-scroll min-h-max">
         <div className="h-16 bg-red-200">test</div>
---
+++ src/utilities/components/nav/Nav.tsx
@@ -0,0 +1,17 @@
+export default function Navigator() {
+  return (
+    <>
+      <input
+        id="navid"
+        className="peer/navid cursor-pointer absolute bottom-5 @[720px]:top-0 right-4 z-10"
+        type="checkbox"
+      />
+      <div className="absolute bottom-0 @[720px]:top-0 h-16  w-full backdrop-blur-md peer-checked/navid:bg-blue-100 bg-opacity-10 peer-checked/navid:animate-height-screen">
+        Nav
+      </div>
+      <div className="absolute hidden bottom-0 right-0 left-0 top-0 backdrop-blur-sm peer-checked/navid:flex p-4 md:p-56">
+        <div className="flex-1 border-2 border-white bg-white bg-opacity-50 rounded-lg  shadow-2xl backdrop-blur-xl"></div>
+      </div>
+    </>
+  );
+}
